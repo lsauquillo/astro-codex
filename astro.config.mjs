ffff2305412504de62f956aeea26b135774e1328
Luis <lsauquillo@gmail.comgit condfig --global user.name lsauquillo>
2026-04-18 12:02:44 +0200

    Add Netlify adapter for dynamic rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import vue from "@astrojs/vue";
+import netlify from "@astrojs/netlify";
 
 export default defineConfig({
+  output: "server",
+  adapter: netlify(),
   integrations: [vue()],
   vite: {
     plugins: [tailwindcss()],
