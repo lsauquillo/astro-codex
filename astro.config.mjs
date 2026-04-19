222fbd9bfb7e4826220fd5e3dd387a7a8aac3e56
Luis <lsauquillo@gmail.comgit condfig --global user.name lsauquillo>
2026-04-19 11:20:07 +0200

    fix: deploy static Astro site on Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,8 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import vue from "@astrojs/vue";
-import netlify from "@astrojs/netlify";
 
 export default defineConfig({
-  output: "server",
-  adapter: netlify(),
   integrations: [vue()],
   vite: {
     plugins: [tailwindcss()],
