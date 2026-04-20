3004246ad36b2bc191efed8f2e1b3a6b2bc8666a
Luis <lsauquillo@gmail.comgit condfig --global user.name lsauquillo>
2026-04-20 10:30:45 +0200

    sitio estatico para subir a netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import vue from "@astrojs/vue";
+import netlify from "@astrojs/netlify";
 
 export default defineConfig({
+  output: "static",
+  adapter: netlify(),
   integrations: [vue()],
   vite: {
     plugins: [tailwindcss()],
